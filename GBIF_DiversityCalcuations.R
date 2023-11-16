32483d515c4761ed7f94ce5c8d6fa25ecdb39b15
kellykapsar <kelly.kapsar@gmail.com>
2023-11-16 22:08:10 +0000

    Add plots and saved output to GBIF div calcs

--- GBIF_DiversityCalcuations.R
+++ GBIF_DiversityCalcuations.R
@@ -10,7 +10,7 @@ setwd("/home/jovyan/data-store/hackathon2023_F")
 gbif_path <- "./Data/plants_in_fire_region.shp"
 fire_path <- "./Data/Rim_fire.shp"
 
-df <- st_read(gbif_path)
+gbif <- st_read(gbif_path)
 fire <- st_read(fire_path) %>% st_transform(st_crs(df))
 
 head(df)
@@ -19,7 +19,7 @@ head(df)
 # Reformat data into format needed by vegan package 
 # According to this tutorial: https://rpubs.com/an-bui/vegan-cheat-sheet
 # (Using year for site since we're calculating biodiversity by year)
-occ <- df %>% 
+occ <- gbif %>% 
   st_drop_geometry() %>% 
   filter(month %in% 4:9) %>% 
   select(species, year) %>% 
@@ -29,15 +29,24 @@ occ <- df %>%
   replace(is.na(.), 0) %>% 
   arrange(., year)
 
-
-
 # For a description of diversity indices see: https://rdrr.io/cran/vegan/man/diversity.html
 df <- data.frame(year = occ$year, 
                  nobs = rowSums(occ[,2:ncol(occ)]), 
                  nspp = vegan::specnumber(occ), 
                  shannon_div = round(vegan::diversity(occ), 3))
 
-ggplot(df, aes(x = year, y = shannon_div)) +
+# Plot results 
+plt1 <- ggplot(df, aes(x = year, y = shannon_div)) +
   geom_line() + 
   xlab("Year") +
   ylab("Shannon Diversity Index")
+ggsave(plt1, "./Figures/GBIF_ShannonIndexThroughTime.png")
+
+
+# Tried making a ggplot and failed. If someone wants to improve, please do! 
+plot(fire$geometry) 
+plot(gbif$geometry, add=T, col="red")
+png("StudyArea_WithGBIF.png")
+
+# Save outputs 
+write.csv(df, "./Data/GBIF_DivByYear_RimFire.csv")
